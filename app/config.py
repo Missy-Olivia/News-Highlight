697380c15c8655c1e9db58a1f94dbb757adb3669
Ngabire Olivia Missy <ngamissia@gmail.com>
2020-11-28 22:48:16 +0200

    added app configuration file

---
+++ app/config.py
@@ -0,0 +1,9 @@
+class Config:
+    News_api_link = 'http://newsapi.org/v2/everything?q=bitcoin&from=2020-10-28&sortBy=publishedAt&apiKey={}'
+
+class DevConfig:
+    debug = True
+
+class ProdConfig:
+
+    pass
