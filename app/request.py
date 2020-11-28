88748c62192d1fee8b30d6699198a7970b121320
Ngabire Olivia Missy <ngamissia@gmail.com>
2020-11-29 01:25:49 +0200

    added news Class and initialised it

--- app/request.py
+++ app/request.py
@@ -1,5 +1,6 @@
 from app import app
 import urllib.request,json
+from news import News
 
 # Getting api key
 api_key = app.config['News_Api_key']
---
+++ app/static/models/news.py
@@ -0,0 +1,8 @@
+class News:
+    def __init__(self, source, author, title, description, news_url, poster):
+        self.source = source
+        self.author = author
+        self.title = title
+        self.description = description
+        self.news_url = news_url
+        self.poster = poster    
