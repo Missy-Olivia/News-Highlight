45e15a44d70f3d525d36a1256c88b581e6732097
Ngabire Olivia Missy <ngamissia@gmail.com>
2020-11-29 21:46:20 +0200

    added get articles function to request file

--- app/request.py
+++ app/request.py
@@ -12,11 +12,11 @@ base_url = app.config["NEWS_SOURCES_BASE_URL"]
 # Getting the articles url
 articles_url = app.config["ARTICLES_API_LINK"]
 
-def my_news():
+def my_news(category):
     '''
-    fx that gives json response to our news request
+    fx that gives json response to our news source request
     '''
-    get_news_url = articles_url.format(api_key)
+    get_news_url = base_url.format(category, api_key)
 
     with urllib.request.urlopen(get_news_url) as url:
         get_news_data = url.read()
@@ -24,18 +24,48 @@ def my_news():
 
         news_results = []
 
-        if get_news_response['articles']:
-            news_results_list = get_news_response['articles']
+        if get_news_response['sources']:
+            news_results_list = get_news_response['sources']
 
         for item in news_results_list:
-            source = item.get('source')
-            author = item.get('author')
-            title = item.get('title')
+            id = item.get('id')
+            name = item.get('name')
             description = item.get('description')
             url = item.get('url')
-            poster = item.get('urlToImage')
+            category = item.get('category')
+            language = item.get('language')
+            country = item.get('country')
 
-            news_object = News(source,author,title,description,url,poster)
+            news_object = News(id,name,description,url,category,language,country)
             news_results.append(news_object)
 
         return news_results
+
+def get_articles(id):
+    '''
+    function that processes articles and returns article object list
+    '''
+    get_articles_url = articles_url.format(id, api_key)
+
+    with urllib.request.urlopen(get_articles_url) as url:
+        get_articles_data = url.read()
+        get_articles_response = json.loads(get_articles_data)
+
+        articles_results = []
+
+        if get_articles_response['articles']:
+            articles_results_list = get_articles_response['articles']
+
+        for articles_item in articles_results_list:
+            id = articles_item.get('id')
+            author = articles_item.get('author')
+            title = articles_item.get('title')
+            description = articles_item.get('description')
+            url = articles_item.get('url')
+            urlToImage = articles_item.get('urlToImage')
+            date = articles_item.get('publishedAt')
+
+            articles_object = Articles(id,author,title,description,url,urlToImage,date)
+            articles_results.append(articles_object)
+
+        return articles_results
--- app/views.py
+++ app/views.py
@@ -1,11 +1,19 @@
 from flask import render_template
 from app import app
-from .request import my_news
+from .request import my_news, get_articles
+from .models import news_source
+News = news_source.News
 
 @app.route('/')
 def index():
-    myNewsData = my_news()
-    print(myNewsData)
+    '''
+	view root page function that returns the index  page and its data
+	'''
+    sources = my_news('business')
+    sports_sources = my_news('sports')
+    tech_sources = my_news('technology')
+    entertainment_sources = my_news('entertainment')
 
     title = "News Highlights"
-    return render_template('index.html',title = title, data = myNewsData)
+
+    return render_template('index.html',title = title, sources = sources,sports_sources = sports_sources,tech_sources = tech_sources,entertainment_sources = entertainment_sources)
