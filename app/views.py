196c4e41e3c873ad6082215726c1d728adbdafac
Ngabire Olivia Missy <ngamissia@gmail.com>
2020-11-30 03:40:33 +0200

    updated html pages to render the news data

--- app/views.py
+++ app/views.py
@@ -9,11 +9,21 @@ def index():
     '''
 	view root page function that returns the index  page and its data
 	'''
-    sources = my_news('business')
+    business_sources = my_news('business')
     sports_sources = my_news('sports')
     tech_sources = my_news('technology')
     entertainment_sources = my_news('entertainment')
 
     title = "News Highlights"
 
-    return render_template('index.html',title = title, sources = sources,sports_sources = sports_sources,tech_sources = tech_sources,entertainment_sources = entertainment_sources)
+    return render_template('index.html',title = title, business_sources = business_sources,sports_sources = sports_sources,tech_sources = tech_sources,entertainment_sources = entertainment_sources)
+
+@app.route('/')
+def articles(id):
+	'''
+	view articles page
+	'''
+	articles = get_articles(id)
+	title = f'NH | {id}'
+
+	return render_template('articles.html',title= title,articles = articles)
