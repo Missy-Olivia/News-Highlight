7a34c8c2cc45dcfca000f56ab1a7d5710b83b2d1
Ngabire Olivia Missy <ngamissia@gmail.com>
2020-12-01 01:16:37 +0200

    initial deployment

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,6 @@ from flask_bootstrap import Bootstrap
 app = Flask(__name__, instance_relative_config= True)
 
 app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
 
 
 # Initializing Flask Extensions
--- app/config.py
+++ app/config.py
@@ -1,6 +1,9 @@
+import os
 class Config:
     ARTICLES_API_LINK = 'http://newsapi.org/v2/everything?domains=wsj.com&apiKey={}'
     NEWS_SOURCES_BASE_URL ='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+
 
 
 
